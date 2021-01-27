6778f4e1dabfb6e2fa2c7a47bbf585ab9adb0d07
AnkurKaushik <a123ankur@yahoo.com>
2021-01-27 15:18:09 -0600

    learning java

---
+++ src/learn.java
@@ -0,0 +1,21 @@
+public class learn {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Implicit Type Casting");
+        char a = 'A';
+        System.out.println("Value of a: "+a);
+
+        int b = a;
+        System.out.println("Value of a: "+b);
+
+        float c = a;
+        System.out.println("Value of a: "+c);
+
+        long d = a;
+        System.out.println("Value of a: "+d);
+
+        double e = a;
+        System.out.println("Value of a: "+e);
+    }
+}
